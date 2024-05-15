ed0bb50a1c8bc7f3781549f4a695227b0f3dd8dd
DumiTanase <103497673+DumiTanase@users.noreply.github.com>
2024-05-15 18:10:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from bs4 import BeautifulSoup
 import requests
 import smtplib
 
-URL = "https://www.amazon.co.uk/Instant-Pot-Electric-Pressure-Stainless/dp/B00OP26T4K/ref=sr_1_4?crid=3TSJEAMJZU1D7&dib=eyJ2IjoiMSJ9.OcFL4PBvLpJp37-eJLpM7sZZ7D99MXoR8ifpY6Lf6UTaV5eUOSxcjXe3iSoKqrJotJs1N_IsafQBYwqeAmgHZ3wht_yJ2Uq1VI1Ocx7yeoxrVAChcpNriTr6XHhRnc-mlvEYXa-8Z6nNLLyTsXnysie_GOcd2F-ZniW4kARWGLwFE1Oj-PwuhVRfbA4IZmu-BbsMB02ilT_IK09suLa8Ywc8SYglOGwrhdjQgHJFAXU.Tbktc7T2ppZoOcaEN1fQj_L6WoVCyMBxVJ9NE0UMTNM&dib_tag=se&keywords=hot%2Bpot%2Bcooker&qid=1713204407&sprefix=hot%2Bpot%2Caps%2C73&sr=8-4&th=1"
+URL = "https://www.amazon.co.uk/Instant-Pot-Electric-Pressure-Sterilizer/dp/B083KM6BZS/ref=sr_1_7?dib=eyJ2IjoiMSJ9.Y9nCxgV7efdzlufj63DqY2ihtVGQE18krUGhY5_o6lvJzFPqNeDX0gFtcVmXthSxJuvRYD4HgQhi3oAVe_bM6YLpA3CF0ezKmFwIjPQe4X5LF-1E1574jADvT4JYQHNOJXn2PEOpRPa9TdRvRG9eCS5eYkQ7d4VD222VjIX-T289eoE-_V8N7ifxmeVVm_58zyFg7MFXbfyohTBo5MLJDAF6lnE4GNc51DcIVusHxzuIaSxv2lgcmscKHMgTEQm01gtPY3yQM4LIY-5ZXQ6fUPvj4kkq9U7inA91iB78e1c.JYoyEDQXeqKHLFhGKWzegxmENFw80djxe2ATlZ_jgFo&dib_tag=se&keywords=instant%2Bpots&qid=1715792979&sr=8-7&th=1"
 headers = {"Accept-Language" : "en,ro;q=0.9,it;q=0.8,es;q=0.7",
-           "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"}
+           "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"}
 response = requests.get(URL, headers=headers)
 html_text = response.text
 soup = BeautifulSoup(html_text, "lxml")
@@ -23,4 +23,4 @@ if price < 100:
         connection.login(user=my_email, password=password)
         connection.sendmail(from_addr=my_email,
                             to_addrs=my_email,
-                            msg="Subject: Price Alert\n\nHellooo")
+                            msg="Subject: Price Alert\n\n Instant Pot Price below 100")
